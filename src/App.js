389e30ed2b8ba39c313a69860c2d039c23cc0bf7
samarthbiraje26 <samarthbiraje@gmail.com>
2025-10-06 13:41:31 +0530

    webpage styling using css

--- src/App.js
+++ src/App.js
@@ -35,7 +35,15 @@ const App = () => {
         </Layout>
       </div>
       <div className='footer'>
-
+        <Typography.Title level={5} style={{color: 'white', textAlign: 'center' }}>
+          CryptoPulse <br />
+          All rights reserved
+        </Typography.Title>
+        <Space>
+          <Link to='/'>Home</Link>
+          <Link to='/exchanges'>Exchanges</Link>
+          <Link to='/news'>News</Link>
+        </Space>
       </div>
     </div>
   );
